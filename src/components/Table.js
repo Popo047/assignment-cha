ea58603b1c856a648c1d39b3393d4f492ace49a0
Mainak Das <mainakdas104@gmail.com>
2021-08-24 00:48:03 +0530

    added table css

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,63 +1,62 @@
-import React, { useState } from "react";
-
+import React, { useState } from 'react';
 function Table(props) {
-  const [timer, setTimer] = useState(Math.floor(Math.random() * 30) + 30);
+	const [timer, setTimer] = useState(Math.floor(Math.random() * 30) + 30);
 
-  setTimeout(() => {
-    setTimer(timer - 1);
-  }, 1000);
+	setTimeout(() => {
+		setTimer(timer - 1);
+	}, 1000);
 
-  return (
-    <div className="container" style={{ paddingTop: "100px" }}>
-      {timer > 0 ? (
-        <p>Time Left : {timer} </p>
-      ) : (
-        <p>Time is Up Please Refresh</p>
-      )}
+	return (
+		<div className='container' style={{ paddingTop: '100px' }}>
+			{timer > 0 ? (
+				<p>Time Left : {timer} </p>
+			) : (
+				<p>Time is Up Please Refresh</p>
+			)}
 
-      <h1>Claim your seats</h1>
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Date</th>
-            <th scope="col">Time</th>
-            <th scope="col">Availability</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.rows.map((row) => (
-            <tr>
-              <th scope="row">{row.id}</th>
-              <td>{row.date}</td>
-              <td>{row.time}</td>
-              <td>{row.availability}</td>
-              <td>
-                {row.availability > 0 ? (
-                  <button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={() => props.onchangeAv(row.id)}
-                  >
-                    Book Now
-                  </button>
-                ) : (
-                  <button
-                    type="button"
-                    disabled
-                    className="btn btn-primary"
-                    onClick={() => props.onchangeAv(row.id)}
-                  >
-                    Full
-                  </button>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+			<h1>Claim your seats</h1>
+			<table>
+				<thead>
+					<tr>
+						<th scope='col'>#</th>
+						<th scope='col'>Date</th>
+						<th scope='col'>Time</th>
+						<th scope='col' colSpan='2'>
+							Availability
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					{props.rows.map((row) => (
+						<tr>
+							<td>{row.id}</td>
+							<td>{row.date}</td>
+							<td>{row.time}</td>
+							<td>{row.availability}</td>
+							<td>
+								{row.availability > 0 ? (
+									<button
+										type='button'
+										className='btn btn-primary'
+										onClick={() => props.onchangeAv(row.id)}>
+										Book Now
+									</button>
+								) : (
+									<button
+										type='button'
+										disabled
+										className='btn btn-primary'
+										onClick={() => props.onchangeAv(row.id)}>
+										Full
+									</button>
+								)}
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default Table;
